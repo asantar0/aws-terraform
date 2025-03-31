160405d15989b869c6530706d0a2724d8a2e902e
Agustin S <agustins@root-view.com>
2025-03-31 18:59:17 -0300

    Lifecycle configuration done in s3 proyect

--- s3/main.tf
+++ s3/main.tf
@@ -1,3 +1,4 @@
+#Basic S3 information in order to create it
 resource "aws_s3_bucket" "my-first-bucket" {
   bucket = var.bucket
 
@@ -7,6 +8,7 @@ resource "aws_s3_bucket" "my-first-bucket" {
   }
 }
 
+#Versioning status
 resource "aws_s3_bucket_versioning" "versioning-parameters" {
   bucket = aws_s3_bucket.my-first-bucket.id
   versioning_configuration {
@@ -14,12 +16,35 @@ resource "aws_s3_bucket_versioning" "versioning-parameters" {
   }
 }
 
+#Encryption Options
 resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_config" {
   bucket = var.bucket
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "s35" {
+  count  = var.lifecycle_enabled ? 1 : 0
+  bucket = var.bucket
+  rule {
+    id = var.s3_id
+    filter {
+      prefix = var.s3_prefix
+    }
+    expiration {
+      days = var.s3_expired_days
+    }
+    status = "Enabled"
+    dynamic "transition" {
+      for_each = var.enable_transition ? [1] : []
+      content {
+        days          = var.s3_transition_days
+        storage_class = var.s3_transtion_storage_class
+      }
     }
   }
 }
--- s3/variables.tf
+++ s3/variables.tf
@@ -3,3 +3,43 @@ variable "bucket" {
   description = "The name of the bucket to create. If omitted, Terraform will assign a random, unique name. Conflicts with name_prefix."
   type        = string
 }
+
+##  This option enables the bucket object to transition to another lowcost storage ##
+variable "enable_transition" {
+  default     = true
+  description = "(Optional) Information in regards to moving the data to low cost storage is required"
+}
+
+variable "s3_transition_days" {
+  type        = number
+  default     = 30
+  description = "How long before it moves to low cost storage"
+}
+
+variable "s3_transtion_storage_class" {
+  type        = string
+  default     = "GLACIER"
+  description = "Which storage it needs to move. i.e Glacier"
+}
+
+variable "s3_id" {
+  default     = "logs"
+  type        = string
+  description = "It can be any meanigful name for us to identify the liefcle policy applied to Bucket"
+}
+
+variable "s3_prefix" {
+  default     = "/"
+  type        = string
+  description = "this option enables to set the rule to specific folder/object within the bucket. / indicates all the objects within the bucket"
+}
+
+variable "s3_expired_days" {
+  default     = null
+  description = "Indicates if the object within bucket needs to expire. default is null so the object never expires"
+}
+
+variable "lifecycle_enabled" {
+default = true
+description = "Does this bucket needs any oject to either transtion to another low cost storage or needs object to be expired after some days"
+}
